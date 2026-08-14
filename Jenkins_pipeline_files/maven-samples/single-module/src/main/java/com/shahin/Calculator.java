93aee03090ecccc4a6e7ce17b3f0f2a84d494125
mshahin3 <mshahin3@yahoo.com>
2026-08-14 11:35:43 -0400

    Add Java class and JUnit test

---
+++ Jenkins_pipeline_files/maven-samples/single-module/src/main/java/com/shahin/Calculator.java
@@ -0,0 +1,8 @@
+package com.shahin;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ Jenkins_pipeline_files/maven-samples/single-module/src/test/java/com/shahin/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.shahin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+
+        Calculator calculator = new Calculator();
+
+        int result = calculator.add(10, 20);
+
+        assertEquals(30, result);
+    }
+}
